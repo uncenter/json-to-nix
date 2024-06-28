cdd85c4210b684e53de7b7a897bef265d8573eff
uncenter <47499684+uncenter@users.noreply.github.com>
2024-06-27 21:12:35 -0400

    refactor: move jsonToNix to separate file

--- src/App.vue
+++ src/App.vue
@@ -26,30 +26,6 @@ const output = ref('');
 const copied = ref(false);
 const clipboard = useClipboard();
 
-function jsonToNix(json: unknown, level: number = 1): string | undefined {
-	const indent = '  '.repeat(level);
-	const subindent = '  '.repeat(level - 1);
-	if (
-		typeof json === 'string' ||
-		Number.isInteger(json) ||
-		json === null ||
-		json === true ||
-		json === false
-	)
-		return `${JSON.stringify(json)}`;
-	else if (Array.isArray(json))
-		return `[\n${json.map((item) => `${indent}${jsonToNix(item, level + 1)}`).join('\n')}\n${subindent}]`;
-	else {
-		let nix = '{\n';
-		for (const [key, value] of Object.entries(
-			json as Record<string, boolean | string | null>,
-		)) {
-			nix += `${indent}${key} = ${jsonToNix(value, level + 1)};\n`;
-		}
-		return nix?.trimEnd() + `\n${subindent}}`;
-	}
-}
-
 async function run() {
 	set(converted, jsonToNix(destr(get(input) || null)));
 	const highlighter = await createHighlighterCore({
---
+++ src/lib.ts
@@ -0,0 +1,23 @@
+function jsonToNix(json: unknown, level: number = 1): string | undefined {
+	const indent = '  '.repeat(level);
+	const subindent = '  '.repeat(level - 1);
+	if (
+		typeof json === 'string' ||
+		Number.isInteger(json) ||
+		json === null ||
+		json === true ||
+		json === false
+	)
+		return `${JSON.stringify(json)}`;
+	else if (Array.isArray(json))
+		return `[\n${json.map((item) => `${indent}${jsonToNix(item, level + 1)}`).join('\n')}\n${subindent}]`;
+	else {
+		let nix = '{\n';
+		for (const [key, value] of Object.entries(
+			json as Record<string, boolean | string | null>,
+		)) {
+			nix += `${indent}${key} = ${jsonToNix(value, level + 1)};\n`;
+		}
+		return nix?.trimEnd() + `\n${subindent}}`;
+	}
+}
