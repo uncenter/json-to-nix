97d5c206d71861a1adfc98b96612257db5efa5ec
uncenter <47499684+uncenter@users.noreply.github.com>
2024-06-27 21:28:36 -0400

    refactor!: separate text input & code output handling from main app component
    Avoids creating a new highlighter instance per change of the text input!

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,7 @@
 <script setup lang="ts">
-import { get, set } from '@vueuse/core';
+import { get } from '@vueuse/core';
 
-import { createHighlighterCore } from 'shiki/core';
-import githubLightTheme from 'shiki/themes/github-light.mjs';
-import githubDarkTheme from 'shiki/themes/github-dark.mjs';
-import nixLang from 'shiki/langs/nix.mjs';
-
-import { destr } from 'destr';
-import { jsonToNix } from './lib';
+import Code from './Code.vue';
 
 const isDarkTheme = useDark();
 const prefersDark = usePreferredDark();
@@ -19,54 +13,6 @@ useFavicon(
 		rel: 'icon',
 	},
 );
-
-const input = useStorage('input', JSON.stringify({ a: true, b: 1, c: 'foo' }));
-const converted = ref('');
-const output = ref('');
-
-const copied = ref(false);
-const clipboard = useClipboard();
-
-async function run() {
-	set(converted, jsonToNix(destr(get(input) || null)));
-	const highlighter = await createHighlighterCore({
-		themes: [githubLightTheme, githubDarkTheme],
-		langs: [nixLang],
-		loadWasm: () => import('shiki/wasm'),
-	});
-
-	set(
-		output,
-		highlighter.codeToHtml(get(converted), {
-			lang: 'nix',
-			theme: get(isDarkTheme) ? 'github-dark' : 'github-light',
-		}),
-	);
-}
-
-function copy() {
-	if (get(converted).length === 0) return;
-	clipboard.copy(get(converted));
-	set(copied, true);
-	setTimeout(() => {
-		set(copied, false);
-	}, 1000);
-}
-
-watch(
-	[input, isDarkTheme],
-	(n, o) => {
-		const changed = o !== n;
-		if (changed) run();
-	},
-	{ immediate: true },
-);
-
-if (import.meta.hot) {
-	import.meta.hot.accept(() => {
-		run();
-	});
-}
 </script>
 
 <template>
@@ -95,51 +41,9 @@ if (import.meta.hot) {
 				</button>
 			</div>
 		</header>
-		<div flex="~ col md:row gap-4" font-mono p4>
-			<textarea
-				v-model="input"
-				border="~ base rounded"
-				placeholder="JSON..."
-				px3
-				py1
-				:class="`${
-					isDarkTheme ? 'bg-[#24292F] ' : ''
-				}w-full md:w-[50vw] h-[40vh] md:h-[70vh]`"
-				spellcheck="false"
-				autocorrect="off"
-				autocapitalize="off"
-			/>
-			<div
-				border="~ base rounded"
-				px3
-				py1
-				relative
-				of-auto
-				:class="`${
-					isDarkTheme ? 'bg-[#24292F] ' : ''
-				}w-full md:w-[50vw] h-[40vh] md:h-[70vh]`"
-			>
-				<button
-					border="~ base rounded"
-					p2
-					absolute
-					top-2
-					right-2
-					:class="
-						converted.length > 0
-							? 'hover:bg-active'
-							: 'op-[0.5] cursor-not-allowed'
-					"
-					:aria-disabled="converted.length === 0"
-					title="Copy"
-					@click="copy()"
-				>
-					<div v-if="copied" i-carbon-checkmark />
-					<div v-else i-carbon-copy />
-				</button>
-				<div v-html="output" />
-			</div>
-		</div>
+		<Suspense>
+			<Code />
+		</Suspense>
 	</div>
 </template>
 
---
+++ src/Code.vue
@@ -0,0 +1,110 @@
+<script setup lang="ts">
+import { get, set } from '@vueuse/core';
+
+import { createHighlighterCore } from 'shiki/core';
+import githubLightTheme from 'shiki/themes/github-light.mjs';
+import githubDarkTheme from 'shiki/themes/github-dark.mjs';
+import nixLang from 'shiki/langs/nix.mjs';
+
+import { destr } from 'destr';
+import { jsonToNix } from './lib';
+
+const isDarkTheme = useDark();
+
+const input = useStorage('input', JSON.stringify({ a: true, b: 1, c: 'foo' }));
+const converted = ref('');
+const output = ref('');
+
+const highlighter = await createHighlighterCore({
+	themes: [githubLightTheme, githubDarkTheme],
+	langs: [nixLang],
+	loadWasm: () => import('shiki/wasm'),
+});
+
+const copied = ref(false);
+const clipboard = useClipboard();
+
+async function run() {
+	set(converted, jsonToNix(destr(get(input) || null)));
+
+	set(
+		output,
+		highlighter.codeToHtml(get(converted), {
+			lang: 'nix',
+			theme: get(isDarkTheme) ? 'github-dark' : 'github-light',
+		}),
+	);
+}
+
+function copy() {
+	if (get(converted).length === 0) return;
+	clipboard.copy(get(converted));
+	set(copied, true);
+	setTimeout(() => {
+		set(copied, false);
+	}, 1000);
+}
+
+watch(
+	[input, isDarkTheme],
+	(n, o) => {
+		const changed = o !== n;
+		if (changed) run();
+	},
+	{ immediate: true },
+);
+
+if (import.meta.hot) {
+	import.meta.hot.accept(() => {
+		run();
+	});
+}
+</script>
+
+<template>
+	<div flex="~ col md:row gap-4" font-mono p4>
+		<textarea
+			v-model="input"
+			border="~ base rounded"
+			placeholder="JSON..."
+			px3
+			py1
+			:class="`${
+				isDarkTheme ? 'bg-[#24292F] ' : ''
+			}w-full md:w-[50vw] h-[40vh] md:h-[70vh]`"
+			spellcheck="false"
+			autocorrect="off"
+			autocapitalize="off"
+		/>
+		<div
+			border="~ base rounded"
+			px3
+			py1
+			relative
+			of-auto
+			:class="`${
+				isDarkTheme ? 'bg-[#24292F] ' : ''
+			}w-full md:w-[50vw] h-[40vh] md:h-[70vh]`"
+		>
+			<button
+				border="~ base rounded"
+				p2
+				absolute
+				top-2
+				right-2
+				:class="
+					converted.length > 0
+						? 'hover:bg-active'
+						: 'op-[0.5] cursor-not-allowed'
+				"
+				:aria-disabled="converted.length === 0"
+				title="Copy"
+				@click="copy()"
+			>
+				<div v-if="copied" i-carbon-checkmark />
+				<div v-else i-carbon-copy />
+			</button>
+			<div v-html="output" />
+		</div>
+	</div>
+</template>
